36220b70263bca0894ce965d320d82035d6d2f71
michael <michaelglenn88@yahoo.com>
2022-03-29 17:59:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record) {
+  for (let el of record) {
+    if (el.result === "W") {
+      return el.year;
+    }
+  }
+  //or
+  //return record.find((object) => object.result === "W")?.year;
+  //or
+  //   function checkYear(year) {
+  //     return year.result === "W";
+  //   }
+  //   const returnYear = record.find(checkYear);
+  //   if (returnYear) {
+  //     return returnYear.year;
+  //   }
+}
